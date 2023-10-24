f622a052e2d1c957fd8b0badbdbd4ef37c13fbab
Lama <lamahamadeh1985@hotmail.com>
2023-10-24 10:24:12 +0100

    Update FromSlopeInterceptToStandradLineEquation.m

--- FromSlopeInterceptToStandradLineEquation.m
+++ FromSlopeInterceptToStandradLineEquation.m
@@ -1,61 +1,73 @@
 
-%convert from slope-intercept line form to standrad line form
+%Code Disclosed | 04/11/2023
+%This code converts from slope-intercept line form to standard line form
 
 %slope-intercept form: 
 % y = mx + d, 
-% where m is the slope and d is the y intercept.
+% where m is the slope and d is the y-intercept.
 
 %standard form:
 %ax + by + c =0
-%where a and b are the x andy coeffieicnts and c is the constant.
+%where a and b are the x and y coefficients and c is the constant.
 %--------------
 
 close all;
 clear all;
+clc
 
-%Define axes
+%Define x axis 
 x = linspace(-10,10,100);
-%Define a point that the line goes through
-r = [1 2];
-%Define an angle between the line and an imaginary horizontal line
-theta = 0.2;
 %--------------
 
-%slope-intercept form: y=slope*x+intercept
-slope = tan(theta);
-intercept = r(2)-slope*r(1);
+%% Slope-Intercept Form
+
+%Define two points that the line goes through
+r1 = [1 2];
+r2 = [-1 0];
+
+%cacluate the slope
+slope = (r2(2)-r1(2))/(r2(1)-r1(1));
+
+%calculate the intercept
+% y = mx + c -> c = y-mx
+intercept = r1(2)-slope*r1(1);
+
+%the stope-intercept line equation
 y1 = slope*x+intercept;
-% visulaise it
+
+% visualize it
 figure;
 plot(x,y1,'b','LineWidth',1.5)
 xlabel('$x$','interpreter','latex')
 ylabel('$y$','interpreter','latex')
-set(gca,'TickLabelInterpreter','latex')
+set(gca,'TickLabelInterpreter','latex','FontSize',12)
 hold on
 %--------------
 
-% convert the decimal slope intp its fraction for using format rational
-[num1, den1] = rat(slope);
-% convert the intercept slope intp its fraction for using format rational
-[num2, den2] = rat(intercept);
-%define the slope using the slope fraction numerator and denominator
-m = num1/den1;
-%define the intercept using the intercept fraction numerator and denominator
-d = num2/den2; %or dem1
-% y = mx+ d
-y2 = m*x+d;
+%% Standard Form
+
+% convert the decimal slope into its fraction format
+[n1, d1] = rat(slope); %n1 is the numerator and d1 is the denominator
+% convert the intercept slope into its fraction format
+[n2, d2] = rat(intercept); %n2 is the numerator and d2 is the denominator
+
+%convert from slope-intercept form to standard form: ax+by+c=0
+a =   n1*d2;
+b = -(d1*d2);
+c =   n2*d1;
+
+% write down the stanrds equation form ax+by+c=0 -> y = (-ax-c)/b
+y2 = -(a*x+c)/b;
+
 %visulaise it
-% plot(x,y2,'rx')
-% hold on
+plot(x,y2,'gx')
+legend('$y=mx+d$','$y=\frac{-ax-c}{b}$','Interpreter','latex')
 %--------------
 
-%convert from slope-intercept form to standard form: ax+bt+c=0
-a =   num1*den2;
-b = -(den1*den2);
-c =   num2*den1;
-% ax+by+c=0 -> y = (-ax-c)/b
-y3 = -(a*x+c)/b;
-%visulaise it
-plot(x,y3,'gx')
-legend('$y=mx+d$','$y=\frac{-ax-c}{b}$','Interpreter','latex')
+%% Error Calculation
+
+%calculate the error between both forms
+mean_err = mean(abs(y1-y2)) ; % mean absolute error 
+MSE      = mean((y1-y2).^2) ; % Mean square error 
+RMSE     = sqrt(mean((y1-y2).^2)) ; % Root mean square error 
 %--------------

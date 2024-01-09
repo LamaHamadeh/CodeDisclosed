6a3c12dc9f9fd2498cc59b1007ed416e869dab55
Lama <lamahamadeh1985@hotmail.com>
2024-01-09 21:39:30 +0000

    Update DotProduct.m

--- DotProduct.m
+++ DotProduct.m
@@ -57,11 +57,11 @@ theta_ob1_deg = rad2deg(theta_ob1); %in degrees
 %---------------------
 %calculate the dot product
 Dot_ob2       = dot(A_ob,B_ob);
-%calculate the norm of A_ac vector
+%calculate the norm of A_ob vector
 A_ob_norm2    = norm(A_ob);
-%calculate the norm of B_ac vector
+%calculate the norm of B_ob vector
 B_ob_norm2    = norm(B_ob);
-% Calculate the angle between A_ac and B_ac
+% Calculate the angle between A_ob and B_ob
 theta_ob2     = acos((Dot_ob2)/(A_ob_norm2*B_ob_norm2)); %in radians
 theta_ob2_deg = rad2deg(theta_ob2); %in degrees
 %--------------------------------------------------------------------------
@@ -74,11 +74,11 @@ B_r = [4, 1, 3 ];
 %----------------
 %calculate the dot product
 Dot_r1       = A_r(1)*B_r(1)+A_r(2)*B_r(2)+A_r(3)*B_r(3);
-%calculate the norm of A_ac vector
+%calculate the norm of A_r vector
 A_r_norm1    = sqrt(A_r(1)^2+A_r(2)^2+A_r(3)^2);
-%calculate the norm of B_ac vector
+%calculate the norm of B_r vector
 B_r_norm1    = sqrt(B_r(1)^2+B_r(2)^2+B_r(3)^2);
-% Calculate the angle between A_ac and B_ac
+% Calculate the angle between A_r and B_r
 theta_r1     = acos((Dot_r1)/(A_r_norm1*B_r_norm1)); %in radians
 theta_r1_deg = rad2deg(theta_r1); %in degrees
 
@@ -86,11 +86,11 @@ theta_r1_deg = rad2deg(theta_r1); %in degrees
 %---------------------
 %calculate the dot product
 Dot_r2       = dot(A_r,B_r);
-%calculate the norm of A_ac vector
+%calculate the norm of A_r vector
 A_r_norm2    = norm(A_r);
-%calculate the norm of B_ac vector
+%calculate the norm of B_r vector
 B_r_norm2    = norm(B_r);
-% Calculate the angle between A_ac and B_ac
+% Calculate the angle between A_r and B_r
 theta_r2     = acos((Dot_r2)/(A_r_norm2*B_r_norm2)); %in radians
 theta_r2_deg = rad2deg(theta_r2); %in degrees
 %--------------------------------------------------------------------------

302dfbce8f2e808b31b7dc81c0811d73807d0f24
Lama <lamahamadeh1985@hotmail.com>
2024-01-20 15:36:44 +0000

    Update CrossProduct.m

--- CrossProduct.m
+++ CrossProduct.m
@@ -1,6 +1,6 @@
 
 %Code Disclosed | 
-%This code calculates the cross product of two vectors and its magnitude 
+%This code calculates the cross-product of two vectors and its magnitude 
 %--------------
 
 clear all;
@@ -18,11 +18,11 @@ close all;
 % A = [3 2 9];
 % B = [1 2/3 3];
 
-% %calculate the cross product between these two vectros
+% %define two vectors (with 180 angle between them, i.e., parallel vectors)
 % A = [5   3  7];
 % B = [-5 -3 -7];
 
-%calculate the angle between both vectors using the dot product
+% Calculate the angle between both vectors using the dot product
 %calculate the dot product
 Dot       = dot(A,B);
 %calculate the norm of A_ob vector
@@ -34,15 +34,15 @@ theta     = acos((Dot)/(A_norm*B_norm)); %in radians
 theta_deg = rad2deg(theta); %in degrees
 %----------------
 
-%caluclate the cross product, i.e., find out the vectro that is
+% calculate the cross product, i.e., find out the vector that is
 %perpendicular to both A and B
 C     = cross(A,B);
-%calculate the magnitude of the cross product
+%calculate the magnitude of the cross-product
 mag_C = norm(A)*norm(B)*abs(sin(theta));
 
 %----------------
 
-%prove that the vector resulting from the dot product is acutally
+%prove that the vector resulting from the dot product is actually
 %perpendicular to both A and B
 % Calculate the angle between C and A
 theta_AC     = acos((dot(A,C))/(norm(A)*norm(C))); %in radians

ca68b7e51ee2cb1cf756b19117072d40c70985e0
Lama <lamahamadeh1985@hotmail.com>
2024-02-06 10:46:55 +0000

    Update TaylorSeries.m

--- TaylorSeries.m
+++ TaylorSeries.m
@@ -40,4 +40,46 @@ legend('$f(x)$', 'Taylor Series: 1 term', 'Taylor Series: 2 terms'...
 set(gca,'TickLabelInterpreter','latex','FontSize',12)
 %------------------------------------------------
 
+%Another example
+%define the spatial variable/coordinates
+x = linspace(-10,10,100);
+%define the exact function we want to approximate
+fx = x.^3-10*x.^2+6;
+%--------------
 
+%visulaise the exact function
+figure;
+plot(x,fx,'c','LineWidth',1)
+xlabel('$x$','interpreter','latex')
+ylabel('$y=f(x)$','interpreter','latex')
+% transparent background
+% set(gcf, 'color', 'none'); 
+% set(gca, 'color', 'none'); 
+% box off
+% axis off
+set(gca,'TickLabelInterpreter','latex','FontSize',12)
+hold on
+
+%--------------
+
+%calulcate the first four terms of the Taylor series around x0=3
+TS1 = -57*ones(1,size(fx,2)); %1 term TS
+TS2 = -33*(x-3); %2 term TS
+TS3 = -(x-3).^2; %3 term TS
+TS4 = (x-3).^3; %4 term TS
+Approx = TS1+TS2+TS3+TS4; %The approximate function calculated using TS
+%--------------
+
+% Visualise the comparison between the exact function of the calculated Taylor
+%series terms
+plot(x,TS1,'r--','LineWidth',1)
+plot(x,TS1+TS2,'g--','LineWidth',1)
+plot(x,TS1+TS2+TS3,'y--','LineWidth',1)
+plot(x,Approx,'m--','LineWidth',1)
+xlabel('$x$','interpreter','latex')
+ylabel('$y=f(x)$','interpreter','latex')
+legend('$f(x)$', 'Taylor Series: 1 term', 'Taylor Series: 2 terms'...
+    , 'Taylor Series: 3 terms', 'Taylor Series: 4 terms',...
+    'location','southeast','interpreter','latex')
+set(gca,'TickLabelInterpreter','latex','FontSize',12)
+%--------------
